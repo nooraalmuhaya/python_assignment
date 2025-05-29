72f551ed396a4b9801cad2a3d0020fe5344ad5e8
nooraalmuhaya <TP088968@mail.apu.edu.my>
2025-05-29 23:43:00 +0800

    fix doctor menu

--- menus/doctor.py
+++ menus/doctor.py
@@ -1,1 +1,23 @@
+def doctor_menu():
+    while True:
+        print("\n--- Doctor Menu ---")
+        print("1. View patient records")
+        print("2. Update medical records")
+        print("3. View appointments")
+        print("4. Block/Unblock availability")
+        print("5. Back to main menu")
 
+        doctor_choice = input("Enter your choice (1-5): ")
+
+        if doctor_choice == '1':
+            view_patient_records() 
+        elif doctor_choice == '2':
+            update_medical_records() 
+        elif doctor_choice == '3':
+            view_appointments()
+        elif doctor_choice == '4':
+            block_unblock_availability()
+        elif doctor_choice == '5':
+            break
+        else:
+            print("Invalid input. Please enter a number between 1 and 5.")
